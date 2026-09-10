430cfa184dd3ebed21a8c1a486824269a0650043
Amy Larson <amylarson@dynamic-141-224-83-25.augsburg.edu>
2026-09-10 11:16:38 -0500

    providing basic main.c

---
+++ programming-csc461f26/project0-embedded101/main.c
@@ -0,0 +1,16 @@
+#define F_CPU 16000000UL   // must be #defined BEFORE including util/delay.h
+#include <avr/io.h>
+#include <util/delay.h>
+
+// Metro built in is Port B pin 5
+#define BOARDLED 5
+
+
+int main(void) {
+
+	// _delay_ms();
+
+    while (1) {
+
+    }
+}
